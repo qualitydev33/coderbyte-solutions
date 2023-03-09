07d9dfbc3ead7c771ba890246ce26fb2806178d7
CAdev1234 <fenglong37@outlook.com>
2023-03-09 00:45:20 -0800

    python age counting solution added

---
+++ python-age-counting.py
@@ -0,0 +1,10 @@
+result = requests.get("https://coderbyte.com/api/challenges/json/age-counting")
+resp_data = result.json()["data"].split(",")
+
+count = 0
+for data in resp_data:
+  split_data = data.split("=")
+  if split_data[0].strip() == "age" and int(split_data[1]) >= 50:
+    count += 1
+
+print(count)
